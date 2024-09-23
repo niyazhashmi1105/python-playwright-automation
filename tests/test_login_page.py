c9b3e444723f3265d5b2001c4ab19d556ea506a9
niyaz <niyaz.hashmi@rediffmail.com>
2024-09-23 18:40:04 +0530

    updated test case

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -9,7 +9,7 @@ def test_login_page(login_page:LoginPage) -> None:
 def test_login_page_locked_user_error(login_page:LoginPage) -> None:
     login_page.go_to_url()
     login_page.do_login('locked_out_user','secret_sauce')
-    assert not login_page.validate_login_error_is_visible() == True,"Error Message is visible"
+    assert login_page.validate_login_error_is_visible() == True,"Error Message is visible"
 
 
 
