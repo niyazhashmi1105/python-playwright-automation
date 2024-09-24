571711fda93b9201d1279befd3c8104f47e693e4
niyaz <niyaz.hashmi@rediffmail.com>
2024-09-24 18:50:40 +0530

    updated dependencies

--- tests/main.py
+++ tests/main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import threading
+from playwright.sync_api import sync_playwright
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Tls(threading.local):
+    def __init__(self) -> None:
+        self.playwright = sync_playwright().start()
+        print("Create playwright instance in Thread", threading.current_thread().name)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+class Worker:
+    tls = Tls()
+
+    def run(self):
+        print("Launched worker in ", threading.current_thread().name)
+        browser = self.tls.playwright.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = browser.new_page()
+        page.goto("http://whatsmyuseragent.org/")
+        page.screenshot(path=f"example-{threading.current_thread().name}.png")
+        page.close()
+        context.close()
+        browser.close()
+        print("Stopped worker in ", threading.current_thread().name)
+
+
+if __name__ == "__main__":
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        for _ in range(10):
+            worker = Worker()
+            executor.submit(worker.run)
